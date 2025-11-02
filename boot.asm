1e27cc975c2ce3bbe78d14b28c2cdf9a39c9cfd9
SumitK06032005 <sumitkumar1year@gmail.com>
2025-11-02 09:29:08 +0000

    Updated boot.asm
    Printing strings in assembly

--- boot.asm
+++ boot.asm
@@ -1,25 +1,24 @@
-;
-; Printing a string 
-;
+org 0x7c00
 
-[org 0x7c00]
-%include 'print_string.asm'
-start :
-    mov bx, MSG1
-    call print_string
+jmp main
 
-    mov bx, MSG2
-    call print_string
+message: db 'Today I am going to learn new things', 0
 
-    jmp $
+print:
+    mov ah, 0eh
+._loop:
+    lodsb
+    cmp al,0
+    je .done
+    int 10h
+    jmp ._loop
+.done:
+    ret
+main:
+    mov si, message
+    call print
+    ret
 
-; Data
-MSG1 :
-    db 'Hello, World',0
 
-MSG2 :
-    db 'Goodbye',0
-
-; Padding and magic number
 times 510-($-$$) db 0
 dw 0xaa55
